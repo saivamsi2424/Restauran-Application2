1d32110917af7f3cb23b6d8536a3f159f540aba6
saivamsi2424 <saivamsi2424@gmail.com>
2025-03-17 13:44:13 +0000

    solution

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import Home from './Home'
+import {Component} from 'react'
+import {Switch, Route} from 'react-router-dom'
+import Home from './components/Home'
+import Login from './components/Login'
+import ProtectedRoute from './components/ProtectedRoute'
+
 import './App.css'
 
 // write your code here
-const App = () => <Home />
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/login" component={Login} />
+        <ProtectedRoute exact path="/" component={Home} />
+      </Switch>
+    )
+  }
+}
 
 export default App
---
+++ src/components/Login/index.js
@@ -0,0 +1,110 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+
+import './index.css'
+
+class Login extends Component {
+  state = {
+    userName: '',
+    password: '',
+    showError: false,
+    errorMessage: '',
+  }
+
+  changeUsername = event => {
+    this.setState({
+      userName: event.target.value,
+    })
+  }
+
+  changePassword = event => {
+    this.setState({
+      password: event.target.value,
+    })
+  }
+
+  renderUserNameField = () => {
+    const {userName} = this.state
+    return (
+      <div>
+        <label className="label" htmlFor="username">
+          USERNAME
+        </label>
+        <input
+          type="text"
+          onChange={this.changeUsername}
+          placeholder="Username"
+          id="username"
+          value={userName}
+        />
+      </div>
+    )
+  }
+
+  renderPasswordField = () => {
+    const {password} = this.state
+    return (
+      <div>
+        <label className="label" htmlFor="password">
+          Password
+        </label>
+        <input
+          type="password"
+          onChange={this.changePassword}
+          placeholder="Username"
+          id="password"
+          value={password}
+        />
+      </div>
+    )
+  }
+
+  onSubmitSucces = jwtToken => {
+    const {history} = this.props
+    Cookies.set('jwt_token', jwtToken, {expires: 30})
+    history.replace('/')
+  }
+
+  onSubmitFailure = errorMessage => {
+    this.setState({showError: true, errorMessage})
+  }
+
+  onFormSubmit = async event => {
+    event.preventDefault()
+    const {userName, password} = this.state
+    const userDetails = {userName, password}
+    const url = 'https://apis.ccbp.in/login'
+
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+
+    const response = await fetch(url, options)
+    const data = await response.json()
+
+    if (response.ok === true) {
+      this.onSubmitSucces(data.jwt_token)
+    } else {
+      this.onSubmitFailure(data.error_msg)
+    }
+  }
+
+  render() {
+    const {errorMessage, showError} = this.state
+    return (
+      <div className="login-form-container">
+        <form className="form" onSubmit={this.onFormSubmit}>
+          {this.renderUserNameField}
+          {this.renderPasswordField}
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+          {!showError && <p>{errorMessage}</p>}
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Login
